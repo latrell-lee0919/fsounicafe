fbc93c13480a74c224c62afd1600e6b728edd6ae
Latrell <lll4xy@virginia.edu>
2021-09-09 15:01:41 -0400

    exercises 1.6 - 1.11

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Heading = (props) => (
+  <h1>{props.text}</h1>
+)
+
+const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text}</button>
+)
+
+
+
+const StatisticLine = (props) => (
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.amount}</td>
+  </tr>
+)
+
+const PositiveReviews = (props) => {
+  //console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <td>positive</td>
+      <td>{props.good / props.total}</td>
+    </tr>
+  )
+}
+  
+
+const AverageReviews = (props) => {
+  console.log(props)
+  return (
+    <tr>
+      <td>average</td>
+      <td>{(props.good - props.bad) / (props.total)}</td>
+    </tr>
+  )
+
+}
+
+
+const TotalReviews = (props) => {
+  console.log(props)
+  return (
+    <tr>
+      <td>all</td>
+      <td>{props.total}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  const total = props.good + props.bad + props.neutral
+  if ((total) !== 0) {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <StatisticLine text="good" amount={props.good} />
+            <StatisticLine text="neutral" amount={props.neutral} />
+            <StatisticLine text="bad" amount={props.bad} />
+            <TotalReviews total={total} good={props.good} bad={props.bad} />
+            <AverageReviews good={props.good} bad={props.bad} total={total} />
+            <PositiveReviews good={props.good} total={total}/>
+          </tbody>
+        </table>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  
+}
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const updateGood = (newValue) => {
+    setGood(newValue)
+  }
+
+  const updateNeutral = (newValue) => {
+    setNeutral(newValue)
+  }
+
+  const updateBad = (newValue) => {
+    setBad(newValue)
+  }
+
+  return (
+    <div>
+      <Heading text="give feedback"/>
+      <Button handleClick={() => updateGood(good + 1)} text="good" />
+      <Button handleClick={() => updateNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => updateBad(bad + 1)} text="bad" />
+      <Heading text="statistics" />
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   );
 }
